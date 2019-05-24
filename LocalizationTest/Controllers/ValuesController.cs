e736534c5c64cd894ac94fa09340dbd138a5b182
陈海荣 <chenhairong@sunlight.bz>
2019-05-24 20:52:21 +0800

    添加项目文件。

---
+++ LocalizationTest/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace LocalizationTest.Abc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IStringLocalizer<ValuesController> _stringLocalizer;
+
+        public ValuesController(IStringLocalizer<ValuesController> stringLocalizer)
+        {
+            _stringLocalizer = stringLocalizer;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { _stringLocalizer["value1"], _stringLocalizer["value2"] };
+        }
+    }
+}
